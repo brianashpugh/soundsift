c673b462fdce7972320d6c3f53342d58f68c632f
Samuel Leggett <samleggett@berkeley.edu>
2014-10-04 02:36:06 -0700

    added functionality to the views interface

--- soundsift_django/soundsift_project/soundsift_project/soundsift_app/views.py
+++ soundsift_django/soundsift_project/soundsift_project/soundsift_app/views.py
@@ -8,6 +8,23 @@ def processUsername(request):
     def availability_query(request):
     username_dict = request.POST.dict()
     username = username_dict["sc-name-in"]
-    followings = client.get('/tracks', limit=10)
+    #this includes the a list with elements as follows:
+    # {artist_user_name : artist's user name,
+    # description: user's description,
+    # full_name: user's full_name}
+    artist_list = {}
+    offset_value = 0
+    while True:
+        followings = client.get('users/' + username + '/followings', offset=offset_value)
+        if len(followings) <= 0:
+            break
+        for artist in followings:
+            artist_list.append = {"artist_user_name": artist.username,
+                                  "description": artist.description}
+        offset_value += 50
+    normalized_list = normalize(artist_list)
+    
+
+
 
 # Create your views here.
