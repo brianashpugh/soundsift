7f51856a7fef9ab540454dde5e5131502474575f
Brian <brian.a.pugh@gmail.com>
2014-10-04 17:20:25 -0700

    sinusoid js written

--- soundsift_django/soundsift_project/soundsift_project/soundsift_app/static/main.js
+++ soundsift_django/soundsift_project/soundsift_project/soundsift_app/static/main.js
@@ -1,8 +1,20 @@
 (function(){
 	window.onload = function() {
+		// wave physics
 		var waveEnergy = 0
 		var decayConstant = 0.8
+
+		// mouse tracking
 		var pastX = 0, pastY = 0
+
+		// sine rendering
+		var xDel = window.innerWidth/20 // 20/4 = 5 periods of the sine wave shown
+		var yAxis = window.innerHeight/2
+		var pathElement = document.getElementById('ola')
+		var currPath = new Array(41)
+		var amplitude = 0
+
+
 		var addEnergy = function(e) {
 			var currX = e.clientX
 			var currY = e.clientY
@@ -13,29 +25,60 @@
 			pastX = currX
 			pastY = currY
 		}
-		window.onmousemove = addEnergy
+
 		var getAmplitude = function() {
 			return waveEnergy
 		}
-		xDel = window.innerWidth/20 //There 10 periods of the sine wave shown
-		yAxis = window.innerHeight/2
 
-		var amplitude = getAmplitude()
-		var currPath = new Array(40)
-		for (var i = 0; i<40; i++) {
-			if i%4 == 1 {
-				currPath[i] = [xDel*i, Math.round(yAxis+amplitude)]
-			}
-			else if i%4 == 3 {
-				currPath[i] = [xDel*i, Math.round(yAxis-amplitude)]
-			}
-			else {
-				currPath[i] = [xDel*i, Math.round(yAxis)]
+		var init = function() {
+			amplitude = getAmplitude()
+			for (var i = 0; i<40; i++) {
+				if i%4 == 1 {
+					currPath[i] = [xDel*i, Math.round(yAxis+amplitude)]
+				}
+				else if i%4 == 3 {
+					currPath[i] = [xDel*i, Math.round(yAxis-amplitude)]
+				}
+				else {
+					currPath[i] = [xDel*i, Math.round(yAxis)]
+				}
 			}
 		}
 
-	var computePath = function(prevPath) {
+		var setPath = function(path) {
+			var pathString = 'M'
+			for (var i=0; i<path.length; i+=2) {
+				if i==0 {
+					pathString += path[i][0].toString() + ',' + path[i][1].toString() + ' '
+					pathString += 'C' + Math.round((path[i][0]+path[i+1][0])/2).toString() + ',' +  path[i+1][1].toString() + ' '
+					pathString += Math.round((path[i+1][0]+path[i+2][0])/2).toString() + ',' + path[i+2][1].toString() + ' '
+					pathString += path[i+2][0].toString() + ',' + path[i+2][1].toString() + ' '
+				}
+				else {
+					pathString += 'C' + Math.round((path[i][0]+path[i+1][0])/2).toString() + ',' +  path[i+1][1].toString() + ' '
+					pathString += Math.round((path[i+1][0]+path[i+2][0])/2).toString() + ',' +  path[i+2][1].toString() + ' '
+					pathString += path[i+2][0].toString() + ',' + path[i+2][1].toString() + ' '
+				}
+			}
+			pathElement.setAttribute('d',pathString)
+		}
 
-		setTimeout(computePath,200)
+		var computePath = function() {
+			amplitude = getAmplitude()
+			for (var i = 0; i<41; i++) {
+				if i%4 == 1 {
+					currPath[i][1] = [Math.round(yAxis+amplitude)]
+				}
+				else if i%4 == 3 {
+					currPath[i][1] = [Math.round(yAxis-amplitude)]
+				}
+			}
+			setPath(currPath)
+			setTimeout(function() {computePath(currPath)},200)
+		}
+
+		window.onmousemove = addEnergy
+		init()
+		computePath()
 	}
 })();
